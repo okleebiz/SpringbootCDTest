8088dd90f422d8394039637c8059415382970bc4
AI-OKLEE <AI-OKLEE@DESKTOP-OLQVOV8>
2022-10-24 00:29:04 +0900

    2차 테스트용 포트 변경

--- Web-Wavic-D-1/src/main/java/com/Wavic/SysD/WebWavicD1Application.java
+++ Web-Wavic-D-1/src/main/java/com/Wavic/SysD/WebWavicD1Application.java
@@ -8,12 +8,12 @@ public class WebWavicD1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebWavicD1Application.class, args);
-		System.out.println("#################Spring Boot 18080 Startup#################");
+		System.out.println("#################Spring Boot 80 Startup#################");
 		System.out.println("#################AP-Check-CPU/MEM#################");
 		System.out.println("#################AP-Resource Check Test#################");
 		System.out.println("################1. Connect-AP-Resource Check Test#################");
 		System.out.println("################2. Connect-AP-Resource Check Test#################");
-		System.out.println("################3.Jenkins TEST_909090");
+		System.out.println("################3.Jenkins TEST_Web Check");
 	}
 
 }
